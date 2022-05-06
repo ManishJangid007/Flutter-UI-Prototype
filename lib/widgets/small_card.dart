a676a06b5da794edc1e9e8c942ab16d8ef66e1a6
Manish <anitasharma4440@gmail.com>
2022-05-06 15:30:27 +0530

    small card icons placed

--- lib/widgets/small_card.dart
+++ lib/widgets/small_card.dart
@@ -44,6 +44,35 @@ class _SmallCardState extends State<SmallCard> {
           ),
         ],
       ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width / 70,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width / 19,
+            height: MediaQuery.of(context).size.height / 11,
+            decoration: BoxDecoration(
+              color: widget.accent_color,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(10.0),
+              ),
+            ),
+            child: Center(
+              child: Icon(
+                widget.icon,
+                color: widget.icon_color,
+                size: MediaQuery.of(context).size.width / 50,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width / 70,
+          ),
+        ],
+      ),
     );
   }
 }
