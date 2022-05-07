c145866422cd69bc30f9e6e5eb2f73ed78d4e05b
Manish <anitasharma4440@gmail.com>
2022-05-07 14:31:50 +0530

    Large Card update

--- lib/widgets/center_row.dart
+++ lib/widgets/center_row.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'large_card.dart';
 import 'medium2_card.dart';
 import 'medium_card.dart';
 import 'small_card.dart';
@@ -67,36 +68,6 @@ class CenterRow extends StatelessWidget {
               MediumCard2(),
             ],
           )
-        ],
-      ),
-    );
-  }
-}
-
-class LargeCard extends StatefulWidget {
-  LargeCard({Key? key}) : super(key: key);
-
-  @override
-  State<LargeCard> createState() => _LargeCardState();
-}
-
-class _LargeCardState extends State<LargeCard> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width / 1.94,
-      height: MediaQuery.of(context).size.height / 2.4,
-      decoration: const BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(
-          Radius.circular(10.0),
-        ),
-        boxShadow: [
-          BoxShadow(
-            offset: Offset(0.0, 0.0),
-            blurRadius: 5.0,
-            color: Color.fromARGB(255, 199, 199, 199),
-          ),
         ],
       ),
     );
